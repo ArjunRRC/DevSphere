4f848358c377a3ab6fca3f934ae26a186ac74f34
Jayashree-S1204 <jayashreeshanmugam12@gmail.com>
2026-06-20 13:58:52 +0530

    Successfully merged contact page files

--- Contact Page/contact.js
+++ Contact Page/contact.js
@@ -48,3 +48,13 @@ git
     });
 
 });
+
+function toggleMenu(){
+    const menu = document.getElementById("mobileMenu");
+
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
